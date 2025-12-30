fe513b8c63c76395e2323b52d6978e3f245f0028
unknown <dima35zotov@mail.ru>
2025-12-30 14:51:49 +0300

    create handlers, list and content from steps

--- instructions-app/src/App.jsx
+++ instructions-app/src/App.jsx
@@ -1,35 +1,113 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import styles from "./app.module.css";
+import data from "./data.json";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+	const [steps, setSteps] = useState(data);
+	const [activeIndex, setActiveIndex] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+	const onPrevButtonClick = () => {
+		setActiveIndex(activeIndex - 1);
+	};
 
-export default App
+	const onNextButtonClick = () => {
+		setActiveIndex(activeIndex + 1);
+	};
+
+	const onResetButtonClick = () => {
+		setActiveIndex(0);
+	};
+
+	let isFirstStep = true;
+	let isLastStep = false;
+
+	return (
+		<div className={styles.container}>
+			<div className={styles.card}>
+				<h1>Инструкция по готовке пельменей</h1>
+				<div className={styles.steps}>
+					<div className={styles["steps-content"]}>
+						{steps[activeIndex].content}
+					</div>
+					<ul className={styles["steps-list"]}>
+						{steps.map((item, index) => (
+							<li className={styles["steps-item"] + " " + styles.done} key={item.id}>
+								<button className={styles["steps-item-button"]}>
+									{index + 1}
+								</button>
+								Шаг {index + 1}
+							</li>
+						))}
+						{/* Выводите <li> с помощью массива steps и метода map(), подставляя в разметку нужные значения и классы */}
+						<li
+							className={styles["steps-item"] + " " + styles.done}
+						>
+							{/* Для того, чтобы вычислить необходимый класс используйте активный индекс, текущий индекс, а также тернарные операторы */}
+							<button className={styles["steps-item-button"]}>
+								1
+							</button>
+							{/* При клике на кнопку установка выбранного шага в качестве активного */}
+							Шаг 1
+						</li>
+						<li
+							className={styles["steps-item"] + " " + styles.done}
+						>
+							<button className={styles["steps-item-button"]}>
+								2
+							</button>
+							Шаг 2
+						</li>
+						<li
+							className={
+								styles["steps-item"] +
+								" " +
+								styles.done +
+								" " +
+								styles.active
+							}
+						>
+							<button className={styles["steps-item-button"]}>
+								3
+							</button>
+							Шаг 3
+						</li>
+						<li className={styles["steps-item"]}>
+							<button className={styles["steps-item-button"]}>
+								4
+							</button>
+							Шаг 4
+						</li>
+					</ul>
+					<div className={styles["buttons-container"]}>
+						<button
+							className={styles.button}
+							onClick={onPrevButtonClick}
+							disabled={isFirstStep}
+						>
+							Назад
+						</button>
+						{!isLastStep ? (
+							<button
+								className={styles.button}
+								onClick={onNextButtonClick}
+							>
+								Далее
+								{/* "Начать сначала", можно сделать этой же кнопкой, просто подменять обработчик и текст в зависимости от условия */}
+								{/* Или заменять всю кнопку в зависимости от условия */}
+							</button>
+						) : (
+							<button
+								className={styles.button}
+								onClick={onResetButtonClick}
+							>
+								Начать сначала
+							</button>
+						)}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default App;
